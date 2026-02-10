dc1e326846addf8ec7f8b24db24d9dfcca28e209
LikaStart <>
2026-02-10 16:38:47 +0000

    Завдання: Підсумковий Проєкт Із Сортування Інвентарю

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
-# Calculate multiplication
-print(7.32 * 0.2) 
-
-# Calculate division
-print(8.30 / 20)
+# Lists of items and categories for slicing
+items = "Bubblegum, Chocolate, Pasta"
+categories = "Candy Aisle, Pasta Aisle"
+candy1 = items [0:9]
+candy2 = items [10:20]
+dry_goods = items [22:]
+#print (candy1 + candy2 + dry_goods)
+category1 = categories [0:11]
+category2 = categories [13:]
+#print (category1 + category2)
+bubblegum_price = "$1.50"
+chocolate_price = "$2.00"
+pasta_price = "$5.40"
+print(f"We have {candy1} for {bubblegum_price} in the {category1}")
+print (f"We have {candy2} for {chocolate_price} in the {category1}")
+print (f"We have {dry_goods} for {pasta_price} in the {category2}")
